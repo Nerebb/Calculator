b7434c986ebbb49b0b96ee945e54e43cfa953365
DESKTOP-3A5KK61\nguye <nguyenhaitan001@gmail.com>
2022-09-24 11:34:43 +0700

    FunctionWorked halfway

--- jarva.js
+++ jarva.js
@@ -1,59 +1,69 @@
-let lastNum = "";
 let num1 = "";
 let num2 = "";
+let operator = "";
 let switchNum = 1;
 const display = document.querySelector(".calculator-display");
 
-//button to CalDisplay, switch = 0 add to num1, switch = 1 add to num2
+//button to CalDisplay, switch = 1 add to num1, switch = 2 add to num2
 const addNumber = (num) => {
-  if (num1 === "" && switchNum === 1) {
+  if ((num1 === "") && (switchNum === 1)) {
     num1 = num;
     display.innerHTML = `${num1}`;
-  } else if (num1 != "" && switchNum === 1) {
+  } else if ((num1 != "") && (switchNum === 1)) {
     num1 = `${num1}${num}`;
     display.innerHTML = `${num1}`;
-  } else if (num2 === "" && switchNum === 2) {
+  } else if ((num2 === "") && (switchNum === 2)) {
     num2 = num;
     display.innerHTML = `${num2}`;
-  } else if (num2 != "" && switchNum === 2) {
+  } else if ((num2 != "") && (switchNum === 2)) {
     num2 = `${num2}${num}`;
     display.innerHTML = `${num2}`;
   }
 };
 
-//Function addNum to operator
-// const addToOperator = () => {
-//   operator = num1;
-// };
-
 //Function plus
 const buttonPlus = () => {
   switchNum = 2;
+  operator = "plus";
   if (num2 != "") {
-    operatorNum = parseInt(num1) + parseInt(num2);
-    num1 = operatorNum;
-    return (display.innerHTML = `${operatorNum}`);
+    num1 = parseInt(num1) + parseInt(num2);
+    return (display.innerHTML = `${num1}`);
   }
 };
 
 //Function minus
-const buttonMinus = (num1, num2) => {
-  lastNum = num1 - num2;
-  return addNumber(lastNum);
+const buttonMinus = () => {
+  switchNum = 2;
+  operator = "minus";
+  if (num2 != "") {
+    num1 = parseInt(num1) - parseInt(num2);
+    return (display.innerHTML = `${num1}`);
+  }
 };
 
 //Function multiply
-const buttonMultiply = (num1, num2) => {
-  lastNum = num1 * num2;
+const buttonMultiply = () => {
+  switchNum = 2;
+  operator = "multiply";
+  if (num2 != "") {
+    num1 = parseInt(num1) * parseInt(num2);
+    return (display.innerHTML = `${num1}`);
+  }
 };
 
 //Function divide
 const buttonDivide = (num1, num2) => {
-  lastNum = num1 / num2;
+  switchNum = 2;
+  operator = "divide";
+  if (num2 != "") {
+    num1 = parseInt(num1) / parseInt(num2);
+    return (display.innerHTML = `${num1}`);
+  }
 };
 
 //Function reset
 const buttonReset = () => {
+  operator = "";
   switchNum = 1;
   num1 = "";
   num2 = "";
@@ -64,9 +74,28 @@ const buttonReset = () => {
 //Function equal
 const buttonEqual = () => {
   switchNum = 1;
-  operatorNum = num1;
-  return (display.innerHTML = `${operatorNum}`);
+  if ((operator === "plus")) {
+    buttonPlus();
+  } else if ((operator === "minus")) {
+    buttonMinus();
+  } else if ((operator === "multiply")) {
+    buttonMultiply();
+  } else if ((operator === "divide")) {
+    buttonDivide();
+  } else {
+    operator = "";
+    display.innerHTML = `${num1}`;
+  }
 };
 
 //Function Delete
-const buttonDelete = () => {};
+const buttonDelete = () => {
+  if (operator != "") {
+    num2 = "";
+    switchNum = 1;
+    operator = "";
+    display.innerHTML = `${num1}`;
+  } else {
+
+  }
+};
